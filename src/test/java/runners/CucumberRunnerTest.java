fcc5daf8d84df0377e275d9177b2ccb24bf37fe4
Germans Veidemans <germans.veidemans@edu.rtu.lv>
2025-05-28 22:59:47 +0300

    runner with changes

--- src/test/java/runners/CucumberRunnerTest.java
+++ src/test/java/runners/CucumberRunnerTest.java
@@ -17,9 +17,10 @@ import io.cucumber.junit.CucumberOptions;
         // feature word - provides location of feature file
         features = "src/test/resources",
         // glue word = provides path to steps definitions class
-        glue = {"stepDefinitions", "hooks"}
-//      ,tags = "(@smoke or @regression) and not (@bug or @wip)"
+        glue = {"stepDefinitions", "hooks"},
+        tags = "@UserStory-1"
         ,monochrome = true
+
 // "@regression" - will run only tests with this tag
 // "@regression and @sanity" - run test which have tags as @regression and @sanity
 // "@regression and not @sanity" - run test which have tags as @regression and NOT @sanity
